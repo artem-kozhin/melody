9e6c2bfe767745295ec192d354e7a6757b3cf9ee
artem-kozhin <95922974+artem-kozhin@users.noreply.github.com>
2021-12-12 21:56:42 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 $(document).ready(function () {
   var currentfloor = 02; //Переменная где хранится текущий этаж
+  var floorPath = $(".home-image path");// каждый отдельный этаж в SVG
   var counterUp = $(".counter-button-up");/*Кнопка увеличения этажа*/
   var counterDown = $(".counter-button-down");/*Кнопка уменьшения этажа */
-  var floorPath = $(".home-image path");// каждый отдельный этаж в SVG
+ 
   
   //функция анимации при наведении мышкой на этаж
   floorPath.on('mouseover',function(){
-      floorPath.removeClass('$("current-floor")');// удаляем анимацию активного этажа
+      floorPath.removeClass("current-floor");// удаляем анимацию активного этажа
       currentfloor = $(this).attr("data-floor"); //Получаем значение текущего этажа
       $(".counter").text(currentfloor);// записываем значение этажа в счетчик справа
   });
@@ -16,17 +17,17 @@ $(document).ready(function () {
       usCurrentFloor = currentfloor.toLocaleString("en-US",{minimumIntegerDigits: 2,
       useGrouping: false}); //форматирование текста чтобы впереди этажа до 10 стоял 0
       $(".counter").text(usCurrentFloor); //записываем значение этажа в счетчик справа
-      floorPath.removeClass('$("current-floor")'); // удаляем анимацию активного этажа
+      $(".home-image path").removeClass("current-floor"); // удаляем анимацию активного этажа
       $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor"); // подсветка текущего этажа
     }
   //Функция отвечающая за уменьшения счетчика этажей  
   });
-  counterDown.on("onclick",function(){
-    if (counter > 2) {currentfloor--;//Проверяем знач этажа больше 2 и уменьшаем 1 этаж
+  counterDown.on("click",function(){
+    if (currentfloor > 2) {currentfloor--; //Проверяем знач этажа больше 2 и уменьшаем 1 этаж
       usCurrentFloor = currentfloor.toLocaleString("en-US",{minimumIntegerDigits: 2,
       useGrouping: false});//форматирование текста чтобы впереди этажа до 10 стоял 0
+      floorPath.removeClass("current-floor");// удаляем анимацию активного этажа
       $(".counter").text(usCurrentFloor);//записываем значение этажа в счетчик справа
-      floorPath.removeClass('$("current-floor")');// удаляем анимацию активного этажа
       $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");// подсветка текущего этажа
     }
   })
